7133059342375b0dd4e3062aab9c056ce7a8d817
Anatoly Boronilov <aboronilov@express42.com>
2024-01-04 17:46:29 +0200

    api created

--- go.mod
+++ go.mod
@@ -5,8 +5,16 @@ go 1.20
 require github.com/joho/godotenv v1.5.1
 
 require (
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.4.3 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+)
+
+require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
-	github.com/gofiber/fiber/v2 v2.51.0 // indirect
+	github.com/gofiber/fiber/v2 v2.51.0
 	github.com/google/uuid v1.4.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
@@ -19,5 +27,6 @@ require (
 	github.com/valyala/fasthttp v1.50.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/sys v0.14.0 // indirect
-	gorm.io/gorm v1.25.5 // indirect
+	gorm.io/driver/postgres v1.5.4
+	gorm.io/gorm v1.25.5
 )
--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/aboronilov/go-fiber-gorm/models"
+	"github.com/aboronilov/go-fiber-gorm/storage"
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
 	"gorm.io/gorm"
@@ -12,7 +16,7 @@ import (
 type Book struct {
 	Author    string `json:"author"`
 	Title     string `json:"title"`
-	Piblisher string `json:"publisher"`
+	Publisher string `json:"publisher"`
 }
 
 type Repository struct {
@@ -66,6 +70,85 @@ func (r *Repository) ListBooks(ctx *fiber.Ctx) error {
 	return nil
 }
 
+func (r *Repository) DeleteBook(ctx *fiber.Ctx) error {
+	bookModel := &models.Books{}
+
+	id := ctx.Params("id")
+	if id == "" {
+		ctx.Status(http.StatusBadRequest).JSON(
+			&fiber.Map{"message": "No id is provided"},
+		)
+		return nil
+	}
+
+	err := r.DB.Delete(bookModel, id).Error
+	if err != nil {
+		ctx.Status(http.StatusBadRequest).JSON(
+			&fiber.Map{"message": "Unable to delete item"},
+		)
+		return err
+	}
+
+	ctx.Status(http.StatusOK).JSON(
+		&fiber.Map{"message": "Item successfully deleted"},
+	)
+	return nil
+}
+
+// func (r *Repository) GetBookByID(ctx *fiber.Ctx) error {
+// 	id := ctx.Params("id")
+// 	if id == "" {
+// 		ctx.Status(http.StatusBadRequest).JSON(
+// 			&fiber.Map{"message": "No id is provided"},
+// 		)
+// 		return nil
+// 	}
+// 	fmt.Println("Get book by id, the id is - ", id)
+
+// 	bookModel := models.Books{}
+// 	err := r.DB.First(&bookModel, id).Error
+// 	if err != nil {
+// 		ctx.Status(http.StatusNotFound).JSON(
+// 			&fiber.Map{"message": "item not found"},
+// 		)
+// 		return err
+// 	}
+
+// 	ctx.Status(http.StatusOK).JSON(
+// 		&fiber.Map{
+// 			"Message": "Item succesfully fetched",
+// 			"Data":    bookModel,
+// 		},
+// 	)
+// 	return nil
+// }
+
+func (r *Repository) GetBookByID(context *fiber.Ctx) error {
+
+	id := context.Params("id")
+	bookModel := &models.Books{}
+	if id == "" {
+		context.Status(http.StatusInternalServerError).JSON(&fiber.Map{
+			"message": "id cannot be empty",
+		})
+		return nil
+	}
+
+	fmt.Println("the ID is", id)
+
+	err := r.DB.Where("id = ?", id).First(bookModel).Error
+	if err != nil {
+		context.Status(http.StatusBadRequest).JSON(
+			&fiber.Map{"message": "could not get the book"})
+		return err
+	}
+	context.Status(http.StatusOK).JSON(&fiber.Map{
+		"message": "book id fetched successfully",
+		"data":    bookModel,
+	})
+	return nil
+}
+
 func (r *Repository) SetupRoutes(app *fiber.App) {
 	api := app.Group("/api/books")
 	api.Post("/", r.CreateBook)
@@ -80,9 +163,22 @@ func main() {
 		log.Fatalln("Error parsing env", err)
 	}
 
+	config := &storage.Config{
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		User:     os.Getenv("DB_USER"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   os.Getenv("DB_NAME"),
+		SSLMode:  os.Getenv("DB_SSL_MODE"),
+	}
 	db, err := storage.NewConnection(config)
 	if err != nil {
 		log.Fatalln("Error connecting to DB...", err)
+	}
+
+	err = models.MigrateBooks(db)
+	if err != nil {
+		log.Fatalln("Error while migrationg...", err)
 	}
 
 	r := Repository{
